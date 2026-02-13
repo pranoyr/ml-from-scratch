0de0d2fecb9fa70fd780db0725d8852a5f9c8b82
Pranoy <pranoyalkr@gmail.com>
2026-02-14 00:41:03 +0530

    init

---
+++ logistic-regression/logistic_regression_model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+from einops import rearrange, reduce, repeat
+
+
+
+def sigmoid(x):
+    return 1 / (1 + torch.exp(-x))
+
+class LogisticRegression(nn.Module):
+    def __init__(self, input_dim):
+        super(LogisticRegression, self).__init__()
+
+        self.weights = nn.Parameter(torch.randn(input_dim))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        # x  -> [B, F]
+        # W -> [F, 1]
+
+
+        # broad cast weights and bias to match the batch size
+        w  = rearrange(self.weights, 'f -> f 1')
+        b = rearrange(self.bias, 'd -> 1 d')
+        z = torch.einsum('i j, j k -> i k', x, w) + b
+        
+        return sigmoid(z)
+    
+x = torch.randn(5, 10)  
+model = LogisticRegression(input_dim=10)
+output = model(x)
+print(output)
